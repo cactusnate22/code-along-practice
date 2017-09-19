d4095122c4ad6bb6a258047c567e9c2af147ae27
Nathan Ash <nash311@gmail.com>
2017-09-19 11:35:47 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let numOne = document.getElementById("num-one");
+let numTwo = document.getElementById("num-two");
+let addSum = document.getElementById("add-sum");
+
+numOne.addEventListener("click", function() {
+	alert("hi");
+});
